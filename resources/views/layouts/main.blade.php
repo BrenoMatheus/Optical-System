a85520277af1caa1eafaeb4f7b877d7d26b94fea
BrenoMatheus <breno.moliver@gmail.com>
2023-04-17 21:38:47 +0100

    Add NavBar Collapse with link menu

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,10 +15,54 @@
         <script src="/js/scripts.js"></script>
         <script src="/js/jquery.js"></script>
         <header>
-            <nav class="navbar bg-light">
-                <a href="/" class="navbar-brand">
-                    <img src="/img/cbv.jpg" class="rounded-circle border border-primary" width="50" alt="Campanha_boa_visao">
-               </a>
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid">  
+                    <a href="/" class="navbar-brand">
+                         <img src="/img/cbv.jpg" class="rounded-circle border border-primary" width="50" alt="Campanha_boa_visao">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/locals">                        
+                            Local
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/pacientes">                        
+                            Pacientes
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/vendas">
+                            Vendas
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/produtos">
+                            Produtos
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/despesas">
+                            Despesas
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/exames">
+                            Exame
+                            </a>
+                        </li> 
+                        <li class="nav-item">
+                            <a class="nav-link" href="/financas">
+                            Financas
+                            </a>
+                        </li>  
+                    </ul>
+                    </div>
+                </div>
             </nav>
         </header>
         <main>
