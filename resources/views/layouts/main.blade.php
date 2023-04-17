b17905da7bee32be15a6bc2b9f0383e0b1f994df
BrenoMatheus <breno.moliver@gmail.com>
2023-04-17 20:51:34 +0100

    Create Navbar with logo-link

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,19 +14,26 @@
 <link rel="stylesheet" href="/css/style.css">
         <script src="/js/scripts.js"></script>
         <script src="/js/jquery.js"></script>
-<main>
-    <div class="container-fluid">
-        <div class="row">
-            @if(session('msg'))                                                    
-            <p class="msg alert alert-success text-center">{{ session('msg') }}</p>
-            @endif
-        @yield('content')   
-    </div>
-    </div>
-</main>      
-<div class="container border-top border-dark">
-    <footer class="py-3 my-4">
-        <p class="text-center text-muted">© 2022 Company, Inc</p>
-    </footer>
-</div>     
+        <header>
+            <nav class="navbar navbar-expand-lg bg-light">
+                <a href="/" class="navbar-brand">
+                    <img src="/img/cbv.jpg" class="rounded-circle border border-primary" width="50" alt="Campanha_boa_visao">
+               </a>
+            </nav>
+        </header>
+        <main>
+            <div class="container-fluid">
+                <div class="row">
+                    @if(session('msg'))                                                    
+                    <p class="msg alert alert-success text-center">{{ session('msg') }}</p>
+                    @endif
+                @yield('content')   
+            </div>
+            </div>
+        </main>      
+        <div class="container border-top border-dark">
+            <footer class="py-3 my-4">
+                <p class="text-center text-muted">© 2022 Company, Inc</p>
+            </footer>
+        </div>     
 </html>
