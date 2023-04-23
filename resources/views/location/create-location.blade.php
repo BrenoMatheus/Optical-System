89688937ddcc40e545ba500d31b33480cd640425
BrenoMatheus <breno.moliver@gmail.com>
2023-04-23 17:46:22 -0300

    add page create-local

---
+++ resources/views/location/create-location.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+@section('title', 'Cadastro de Local')
+@section('content')
+
+<div class="container p-4 ">
+        <div class="card m-5">     
+            <div class="card-header">
+                <h4 class="card-title text-center fw-bold">Local</h4>           
+              </div>
+              <div class="card-body">       
+                
+            </div>
+        </div>
+</div>        
+
+@endsection
