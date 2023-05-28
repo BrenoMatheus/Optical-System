9dfe5f6c597d930c56e71d7716e83568d0e36ae1
BrenoMatheus <breno.moj99@gmail.com>
2023-05-28 15:11:36 -0300

    add page to edit location

---
+++ resources/views/location/edit-local.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.main')
+@section('title', 'Edição de Local')
+@section('content')
+
+<div class="container p-4 ">
+        <div class="card m-5">     
+            <div class="card-header">
+                <h4 class="card-title text-center fw-bold">Local</h4>           
+              </div>
+              <form action="/local/update/{{$local->id}}" method="POST" enctype="multipart/form-data">
+                @csrf
+                @method('PUT') 
+              <div class="card-body"> 
+              <div class="row p-3 justify-content-center">                  
+                  <div class="col">
+                    <label class="form-label fw-bold">local:</label>
+                    <input type="text" class="form-control" name="local" id="local" value="{{$local->local}}" required>
+                  </div>  
+                  <div class="col">
+                  <label for="validationCustom03" class="form-label fw-bold">Contato Principal:</label>
+                  <input type="text" class="form-control" name="contato" id="contato" value="{{$local->contato}}" required>
+                </div>                 
+              </div>
+               <div class="row p-3 justify-content-center">                  
+                   <div class="col">
+                    <label class="form-label fw-bold">Observação:</label>
+                    <input type="text" class="form-control" name="observacao" id="observacao" value="{{$local->endereco}}" >                   
+                  </div> 
+                  <div class="col">
+                    <label class="form-label fw-bold">telefone Principal:</label>
+                    <input type="number" class="form-control" name="telefone" id="telefone" value="{{$local->telefone}}" required>                   
+                  </div>                
+              </div>
+                 <div class="row p-3 justify-content-center">
+                        <button type="submit" class="btn btn-warning">Editar</button>
+                 </div>
+                 </form>
+            </div>
+        </div>
+
+@endsection
