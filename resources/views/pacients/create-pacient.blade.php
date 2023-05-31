a7deb2f21abb99150cf552de32e35b49af793721
BrenoMatheus <breno.moj99@gmail.com>
2023-05-31 08:49:13 -0300

    page for patient registration

---
+++ resources/views/pacients/create-pacient.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+@section('title', 'Cadastro de Paciente')
+@section('content')
+<div class="container p-4 ">
+        <div class="card m-5">     
+            <div class="card-header">
+                <h4 class="card-title text-center fw-bold">Paciente</h4>           
+              </div>
+              <div class="card-body">       
+            
+            </div>
+        </div>
+@endsection
