438f75137793741f7810761cac0160bee3129e17
BrenoMatheus <breno.moj99@gmail.com>
2023-06-05 22:01:55 -0300

    add examination creation page extending the layout of page show-pacient

---
+++ resources/views/exame/create-exame.blade.php
@@ -0,0 +1,9 @@
+@extends('pacients.show-pacient')
+@section('title', 'Exame:' .$pacient->nome)
+@section('exame')
+
+        <div class="card text-center fw-bold">
+        
+        </div>  
+
+@endsection
