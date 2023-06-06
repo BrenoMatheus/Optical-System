8741aa6008bab4dd5ecadedf54870d2e8184a313
BrenoMatheus <breno.moj99@gmail.com>
2023-06-05 21:53:49 -0300

    function to redirect to the examination creation page

--- app/Http/Controllers/ExameController.php
+++ app/Http/Controllers/ExameController.php
@@ -12,5 +12,12 @@ use App\Models\Local;
 
 class ExameController extends Controller
 {
-    //
+    
+    public function create($id){
+        $disabled = '1'; // constant defined to disable the buttons on the show-pacient page.
+        $pacient = Pacient::findOrFail($id); // search for data of the patient who will be examined
+        $local = Local::where('id', $pacient->local_id)->first();   // search for data of the local who will be examined
+        $user = auth()->user();      
+        return view('exame.create-exame',['pacient' => $pacient,'local' => $local, 'user'=> $user->name,'disabled' => $disabled]);
+    }
 }
